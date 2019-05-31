e96defd32fb5deabcc4a486e4d42e6c0534e0687
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-31 16:11:50 +0300

    adds database rule in order to connect the test database to the tests and clear that data after each test

---
+++ src/test/java/DataBaseRule.java
@@ -0,0 +1,26 @@
+import org.sql2o.*;
+import org.junit.rules.ExternalResource;
+
+public class DataBaseRule extends ExternalResource{
+
+    @Override
+    protected void before(){
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "mbugua", "4545");
+    }
+
+    @Override
+    protected void after(){
+        try(Connection con = DB.sql2o.open()){
+            String sqlSpecies = "DELETE FROM species *; ";
+            String sqlAnimals = "DELETE FROM animals *;";
+            String sqlRangers = "DELETE FROM rangers *;";
+            String sqlLocation = "DELETE FROM locations *;";
+            String sqlSightings = "DELETE FROM sightings *;";
+            con.createQuery(sqlAnimals).executeUpdate();
+            con.createQuery(sqlLocation).executeUpdate();
+            con.createQuery(sqlRangers).executeUpdate();
+            con.createQuery(sqlSightings).executeUpdate();
+            con.createQuery(sqlSpecies).executeUpdate();
+        }
+    }
+}
