07d142b44285b99d27f3ec7b4894ca37c8c006f6
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-06-02 19:49:26 +0300

    create the sighting class with all its attributes, getter methods, CRUD functionalities and all corresponding tests

---
+++ src/main/java/Sighting.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+import org.sql2o.*;
+import java.util.List;
+
+public class Sighting{
+    private int animal_id;
+    private int location_id;
+    private int ranger_id;
+    private int species_id;
+
+    private int id;
+
+
+    public Sighting(int animal_id,int location_id, int ranger_id,int species_id){
+       this.animal_id = animal_id;
+       this.location_id = location_id;
+       this.ranger_id = ranger_id;
+        this.species_id = species_id;
+       
+    }
+
+    public int getAnimal_id() {
+        return animal_id;
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public int getRanger_id() {
+        return ranger_id;
+    }
+
+    public int getSpecies_id(){
+        return species_id;
+    }
+
+  
+
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object otherSighting){
+        if(!(otherSighting instanceof Sighting)){
+            return false;
+        } else {
+            Sighting sighting = (Sighting) otherSighting;
+            return this.getAnimal_id() == sighting.getAnimal_id() &&
+                    this.getLocation_id() == sighting.getLocation_id() &&
+                    this.getRanger_id() == sighting.getRanger_id() &&
+                    this.getSpecies_id() == sighting.getSpecies_id();
+                    
+        }
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings (animal_id, location_id, ranger_id, species_id) VALUES (:animal_id, :location_id, :ranger_id, :species_id)";
+            this.id =(int) con.createQuery(sql, true)
+                    .addParameter("animal_id", this.animal_id)
+                    .addParameter("location_id", this.location_id)
+                    .addParameter("ranger_id", this.ranger_id)
+                    .addParameter("species_id", this.species_id)
+                    .executeUpdate()
+                    .getKey();
+
+        }
+    }
+
+    public static List<Sighting> all(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM sightings";
+            return    con.createQuery(sql)
+                    .executeAndFetch(Sighting.class);
+        }
+    }
+    public static Sighting find(int id){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM sightings WHERE id =:id";
+            Sighting sighting  = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sighting.class);
+            return sighting;
+        }
+    }
+    public void update(int animal_id, int location_id, int ranger_id,  int species_id){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "UPDATE sightings SET animal_id = :animal_id, species_id = :species_id, location_id = :location_id, ranger_id = :ranger_id WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("animal_id", animal_id)
+                    .addParameter("species_id", species_id)
+                    .addParameter("location_id", location_id)
+                    .addParameter("ranger_id" , ranger_id)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+}
---
+++ src/test/java/SightingTest.java
@@ -0,0 +1,80 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SightingTest{
+
+    @Rule
+    public DataBaseRule dataBaseRule = new DataBaseRule();
+
+    @Test
+    public void sighting_instantiatesCorrectly_true(){
+        Sighting sighting = new Sighting (1,2,3,4);
+        assertTrue(sighting instanceof Sighting);
+    }
+
+    @Test
+    public void sighting_instantiatedWithAnimal_Id(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        assertEquals(1, sighting.getAnimal_id());
+    }
+
+    @Test
+    public void sighting_instantiatesWithSpecies_id(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        assertEquals(4, sighting.getSpecies_id());
+    }
+
+    @Test
+    public void sighting_instantiatesWithLocation_Id(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        assertEquals(2, sighting.getLocation_id());
+    }
+    @Test
+    public void sighting_instantiatesWithRanger_Id(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        assertEquals(3, sighting.getRanger_id());
+    }
+    @Test
+    public void equals_returnsTrueIfObjectsAreEquals(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        Sighting sightingtwo =  new Sighting (1,2,3,4);
+        assertTrue(sighting.equals(sightingtwo));
+    }
+
+    @Test
+    public void save_savesObjectIntoDatabase(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        sighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(sighting));
+    }
+
+    @Test
+    public void all_retrievesAllInstancesFromTheDatabase(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        sighting.save();
+        Sighting sightingTwo =  new Sighting (2,3,1,4);
+        sightingTwo.save();
+        assertTrue(Sighting.all().get(0).equals(sighting));
+        assertTrue(Sighting.all().get(1).equals(sightingTwo));
+
+    }
+
+    @Test
+    public void find_retrievesASpecificSightingUsingItsId(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        sighting.save();
+        Sighting sightingTwo =  new Sighting (2,3,4,5);
+        sightingTwo.save();
+        assertEquals(sightingTwo.getAnimal_id(), Sighting.find(sightingTwo.getId()).getAnimal_id());
+    }
+
+    @Test
+    public void update_updatesAnSightingDetails(){
+        Sighting sighting =  new Sighting (1,2,3,4);
+        sighting.save();
+        sighting.update(2,3,4,5);
+        assertEquals(4, Sighting.find(sighting.getId()).getRanger_id());
+    }
+
+
+}
