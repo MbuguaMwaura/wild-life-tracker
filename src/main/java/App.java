f634cb9bb8071db12b3e17096bdf1ba766c0ccb3
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-06-03 07:23:59 +0300

    adds the ranger, animal and species forms with their base styling to the /form page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,12 @@ public class App{
             return new ModelAndView(model,layout);
         }, new VelocityTemplateEngine());
 
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/form.vtl");
+            return new ModelAndView(model,layout);
+        }, new VelocityTemplateEngine());
+
     }
 
 
